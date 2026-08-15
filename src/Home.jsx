0979caba9e8ea2069d33393597324740de1d44e1
Satyamrk <satyamrk18@gmail.com>
2026-08-16 00:00:41 +0530

    side bar added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import wallpaper from "./images/wallpaper.png";
 import NavBar from "./components/NavBar";
-
+import SideBar from "./components/SideBar.jsx";
 function Home() {
   return (
     <div className="h-screen w-full flex flex-col">
@@ -9,9 +9,14 @@ function Home() {
       </div>
 
       <div
-        className="h-[97%] w-full bg-cover bg-center bg-no-repeat"
+        className="h-[97%] w-full bg-cover bg-center bg-no-repeat flex items-center"
         style={{ backgroundImage: `url(${wallpaper})` }}
-      ></div>
+      >
+        <div className="h-full w-[5%]">
+          <SideBar />
+        </div>
+        <div className="h-full w-[95%]"></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,8 @@
+function SideBar() {
+  return (
+    <div className="h-full w-full border-2">
+      <h1 className="text-cyan-500">hello</h1>
+    </div>
+  );
+}
+export default SideBar;
