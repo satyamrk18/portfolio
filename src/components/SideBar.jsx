04ca5338ac241b2b4737cea1651cbead5b4bab11
Satyamrk <satyamrk18@gmail.com>
2026-08-16 00:46:14 +0530

    side navbar all content added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,19 +1,23 @@
 import firefox from "./../images/firefox.png";
+import computer from "./../images/computer.png";
 import github from "./../images/github.png";
+import leetcode from "./../images/leetcode.png";
+import linkedin from "./../images/linkedin.webp";
+import peerlist from "./../images/peerlist.webp";
+import keypad from "./../images/keypad.png";
 function SideBar() {
   return (
     <div className="h-full w-full bg-gray-950/50 flex justify-center flex-col">
       <div className="h-[70%] flex flex-col items-center justify-evenly">
-        <img src={firefox} className="h-[50px]" />
-        <img src={github} className="h-[50px]" />
-        <img src={firefox} className="h-[50px]" />
-        <img src={firefox} className="h-[50px]" />
-        <img src={firefox} className="h-[50px]" />
-        <img src={firefox} className="h-[50px]" />
-        <img src={firefox} className="h-[50px]" />
+        <img src={firefox} className="h-[50px] in-hover:cursor-pointer" />
+        <img src={computer} className="h-[50px] in-hover:cursor-pointer" />
+        <img src={github} className="h-[50px] in-hover:cursor-pointer" />
+        <img src={leetcode} className="h-[55px] in-hover:cursor-pointer" />
+        <img src={linkedin} className="h-[55px] in-hover:cursor-pointer" />
+        <img src={peerlist} className="h-[50px] in-hover:cursor-pointer" />
       </div>
-      <div className="h-[30%] border-2 ">
-        <h1>hii</h1>
+      <div className="h-[30%] flex items-end justify-center ">
+        <img src={keypad} className="h-[40px] mb-10" />
       </div>
     </div>
   );
