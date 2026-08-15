5d282ad8b6e918a8d32cf6172fe70f2d608e2bc5
Satyamrk <satyamrk18@gmail.com>
2026-08-16 00:16:49 +0530

    side bas adjustment done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,20 @@
+import firefox from "./../images/firefox.png";
+import github from "./../images/github.png";
 function SideBar() {
   return (
-    <div className="h-full w-full border-2">
-      <h1 className="text-cyan-500">hello</h1>
+    <div className="h-full w-full bg-gray-950/50 flex justify-center flex-col">
+      <div className="h-[70%] flex flex-col items-center justify-evenly">
+        <img src={firefox} className="h-[50px]" />
+        <img src={github} className="h-[50px]" />
+        <img src={firefox} className="h-[50px]" />
+        <img src={firefox} className="h-[50px]" />
+        <img src={firefox} className="h-[50px]" />
+        <img src={firefox} className="h-[50px]" />
+        <img src={firefox} className="h-[50px]" />
+      </div>
+      <div className="h-[30%] border-2 ">
+        <h1>hii</h1>
+      </div>
     </div>
   );
 }
