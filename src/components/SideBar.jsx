35dbc635594fe699a9a5f319f6474c0d4c34bd7b
Satyamrk <satyamrk18@gmail.com>
2026-08-16 00:52:50 +0530

    adjustment done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,13 +7,13 @@ import peerlist from "./../images/peerlist.webp";
 import keypad from "./../images/keypad.png";
 function SideBar() {
   return (
-    <div className="h-full w-full bg-gray-950/50 flex justify-center flex-col">
+    <div className="h-full w-full bg-gray-950/50 flex  justify-center flex-col">
       <div className="h-[70%] flex flex-col items-center justify-evenly">
         <img src={firefox} className="h-[50px] in-hover:cursor-pointer" />
         <img src={computer} className="h-[50px] in-hover:cursor-pointer" />
         <img src={github} className="h-[50px] in-hover:cursor-pointer" />
-        <img src={leetcode} className="h-[55px] in-hover:cursor-pointer" />
-        <img src={linkedin} className="h-[55px] in-hover:cursor-pointer" />
+        <img src={leetcode} className="h-[65px] in-hover:cursor-pointer" />
+        <img src={linkedin} className="h-[65px] in-hover:cursor-pointer" />
         <img src={peerlist} className="h-[50px] in-hover:cursor-pointer" />
       </div>
       <div className="h-[30%] flex items-end justify-center ">
